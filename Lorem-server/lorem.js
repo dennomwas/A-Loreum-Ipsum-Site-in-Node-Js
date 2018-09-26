d94a03ffb87285758f005ff4795960ebd7ed76ea
dennomwas <githinji.mwangi@gmail.com>
2018-09-26 11:02:38 +0300

    [Ft] create lorem ipsum server and helper functions

---
+++ Lorem-server/lorem.js
@@ -0,0 +1,32 @@
+let lorem = require('./lorem-data');
+
+// sentences
+const getSentence = (noOfLines) => {
+    let splitSentences = lorem.loremIpsumData.split(".");
+
+    const sentences = splitSentences.map(sentence => {
+        return sentence.trim();
+    });
+    return sentences.slice(0, noOfLines);
+}
+
+// paragraphs
+const getParagraph = (noOfParagraphs) => {
+    let splitParagraphs = lorem.loremIpsumData.split("\n");
+
+    const paragraphs = splitParagraphs.filter((paragraph, index) => {
+        return paragraph[index];
+    });
+    return paragraphs.slice(0, noOfParagraphs)
+
+}
+
+// words
+const getWords = (noOfWords) => {
+    let splitWords = lorem.loremIpsumData.split(" ");
+
+    const words = splitWords.map(word => {
+        return word.replace(/[,.]/g, '');
+    })
+    return words.splice(0, noOfWords);
+}
