f5bc1f9eee8fa5a239c44f1a0e96e89f3b2443f6
dennomwas <githinji.mwangi@gmail.com>
2018-09-28 01:49:13 +0300

    [Ch] refactor the server

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let http = require('http');
-let hostname = '127.0.0.1';
-let port = 3000;
+const http = require('http');
+
+// local imports
+const route = require('./routes.js');
+
+//variables
+const hostname = '127.0.0.1';
+const port = '3000';
 
 http.createServer((request, response) => {
-
+    route.home(request, response);
 }).listen(port, hostname);
 console.log(`Server running at ${hostname}:${port}`);
