d0fb2a1988bcc54eed14890ce25dadeff71e2f38
dennomwas <dennomwas@users.noreply.github.com>
2018-12-21 09:37:33 +0300

    Fix semi-colons

--- routes.js
+++ routes.js
@@ -8,26 +8,26 @@ const {
 } = require('./Lorem-server/lorem');
 const render = require("./renderer.js");
 let header = {
-    "Content-Type": "text/html"
+    "Content-Type": "text/html";
 };
 
 const homeRoute = (request, response) => {
     if (request.url === "/") {
         if (request.method === "GET") {
-            response.writeHead(200, header)
+            response.writeHead(200, header);
             render.view("header", null, response);
             render.view("body", null, response);
             response.end();
         } else {
             if (request.method === 'POST') {
                 request.on('data', (chunk) => {
-                    let query = querystring.parse(chunk.toString())
+                    let query = querystring.parse(chunk.toString());
                     if (query.number > 0) {
                         if (query.button === "parachecker") {
                             // get the data
                             const loremData = loremParagraph(query.number);
                             // write to template
-                            response.writeHead(200, header)
+                            response.writeHead(200, header);
                             render.view("header", null, response);
                             render.view("body", null, response);
                             render.view("details", loremData, response);
@@ -38,7 +38,7 @@ const homeRoute = (request, response) => {
                             // get the data
                             const loremData = loremSentence(query.number);
                             // write to template
-                            response.writeHead(200, header)
+                            response.writeHead(200, header);
                             render.view("header", null, response);
                             render.view("body", null, response);
                             render.view("details", loremData, response);
@@ -49,7 +49,7 @@ const homeRoute = (request, response) => {
                             // get the data
                             const loremData = loremWord(query.number);
                             // write to template
-                            response.writeHead(200, header)
+                            response.writeHead(200, header);
                             render.view("header", null, response);
                             render.view("body", null, response);
                             render.view("details", loremData, response);
@@ -67,8 +67,8 @@ const homeRoute = (request, response) => {
                     }
                 });
                 request.on("error", () => {
-                    response.writeHead(200, header)
-                    response.write("There was an error with your request!")
+                    response.writeHead(200, header);
+                    response.write("There was an error with your request!");
                     render.view("body", null, response);
                     response.end();
                 })
